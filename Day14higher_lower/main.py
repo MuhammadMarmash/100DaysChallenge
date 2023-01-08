61701021b6ecccde31f5fb512afcaea33b49503b
MuhammadMarmash <hamada500014@gmail.com>
2023-01-08 14:21:11 +0200

    day15

--- Day14higher_lower/main.py
+++ Day14higher_lower/main.py
@@ -1,13 +1,13 @@
-import art
-import game_data
+from art import *
+from game_data import *
 import random
 
 
 def random_choice():
-    random1 = random.randint(0, len(game_data.data) - 1)
-    random2 = random.randint(0, len(game_data.data) - 1)
+    random1 = random.randint(0, len(data) - 1)
+    random2 = random.randint(0, len(data) - 1)
     while random2 == random1:
-        random2 = random.randint(0, len(game_data.data) - 1)
+        random2 = random.randint(0, len(data) - 1)
     return random1, random2
 
 
@@ -15,19 +15,19 @@ end = False
 score = 0
 while not end:
     choice1, choice2 = random_choice()
-    print(art.logo)
+    print(logo)
     if score > 0:
         print(f"You're right! Current score: {score}.")
     print(
-        f"Compare A: {game_data.data[choice1]['name']}, {game_data.data[choice1]['description']},"
-        f" from {game_data.data[choice1]['country']}")
-    print(art.vs)
+        f"Compare A: {data[choice1]['name']}, {data[choice1]['description']},"
+        f" from {data[choice1]['country']}")
+    print(vs)
     print(
-        f"Against B: {game_data.data[choice2]['name']}, {game_data.data[choice2]['description']},"
-        f" from {game_data.data[choice2]['country']}")
+        f"Against B: {data[choice2]['name']}, {data[choice2]['description']},"
+        f" from {data[choice2]['country']}")
     print("Who has more followers? Type 'A' or 'B':")
     answer = str(input()).lower()
-    correct_answer = game_data.data[choice1]['follower_count'] > game_data.data[choice2]['follower_count']
+    correct_answer = data[choice1]['follower_count'] > data[choice2]['follower_count']
     if (correct_answer and answer == "a") or (not correct_answer and answer == "b"):
         score += 1
         print("\n" * 80)
