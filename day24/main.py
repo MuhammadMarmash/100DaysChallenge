db834f736b553194bdbd0082f81a21ab17bd3764
MuhammadMarmash <hamada500014@gmail.com>
2023-01-16 12:46:37 +0200

    day24

---
+++ day24/main.py
@@ -0,0 +1,11 @@
+with open(file="Input/Names/invited_names.txt") as file:
+    # names = file.read().split("\n")
+    names = file.readlines()
+
+with open(file="Input/Letters/starting_letter.txt") as file:
+    text = file.read()
+
+for name in names:
+    new_text = text.replace("[name]", name)
+    with open(file=f"Output/ReadyToSend/letter_for_{name}", mode="w") as file:
+        file.write(new_text)
