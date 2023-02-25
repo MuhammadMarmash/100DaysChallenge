2a7003fcec07fee94f4e28d7faa61c23735227d3
MuhammadMarmash <hamada500014@gmail.com>
2023-02-25 09:18:23 +0200

    day57

---
+++ days54-70_WebDev/day57/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+response.raise_for_status()
+all_blogs = response.json()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", blogs=all_blogs)
+
+
+@app.route('/blog/<int:id>')
+def the_blog(id):
+    for blog in all_blogs:
+        if blog["id"] == id:
+            return render_template("post.html", blog=blog)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
